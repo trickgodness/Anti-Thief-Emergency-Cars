9a47f1bdb3c316436231fb90013b58fe27ee89ba
TheRealCasiv2 <60825241+TheRealCasiv2@users.noreply.github.com>
2022-07-04 02:21:27 +0200

    fix mistakes

--- server.lua
+++ server.lua
@@ -1,8 +1,4 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 RegisterServerEvent('kickplayer')
-AddEventHandler('kickplayer', function(target)
-    local src = source
-    DropPlayer(target, 'You Have Been Kicked Server Because Thiefing Emergency Cars')
+AddEventHandler('kickplayer', function()
+    DropPlayer(source, 'You Have Been Kicked Server Because Thiefing Emergency Cars')
 end)
