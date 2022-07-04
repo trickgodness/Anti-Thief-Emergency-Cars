64d87d1c6155e91771f2907cabfe1937054bb29f
TheRealCasiv2 <60825241+TheRealCasiv2@users.noreply.github.com>
2022-07-04 02:20:17 +0200

    fix mistakes

--- client.lua
+++ client.lua
@@ -1,35 +1,19 @@
-ESX = nil
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-
-	while ESX.GetPlayerData().job == nil do
-		Citizen.Wait(0)
-	end
-
-	PlayerData = ESX.GetPlayerData()
-
-	
-end)
+local currentJob = nil
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-	ESX.PlayerData = xPlayer
+	currentJob = xPlayer.job.name
 end)
 
 RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
-	PlayerData.job = job
+	currentJob = job
 end)
 
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
         local ped = PlayerPedId()
-		local PlayerId = GetPlayerServerId(PlayerId())
         local veh = GetVehiclePedIsIn(ped, false)
         if veh ~= nil then
             if GetVehicleClass(veh) == 18 or GetVehicleClass(veh) == 15 then
@@ -37,7 +21,7 @@ Citizen.CreateThread(function()
 					if (GetPedInVehicleSeat(veh, -1) == ped) then
 						TaskLeaveVehicle(ped,veh,0)
 						if Config.KickPlayer then
-							TriggerServerEvent('kickplayer', PlayerId)
+							TriggerServerEvent('kickplayer')
 						end
 					end
 				end        
